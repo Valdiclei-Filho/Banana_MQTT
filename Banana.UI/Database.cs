47dd2adae7bb4eda9ae827b63c0a3931f53ad873
Valdiclei <cacausoares474@gmail.com>
2024-09-25 21:40:07 -0300

    melhoria

--- Banana.UI/Database.cs
+++ Banana.UI/Database.cs
@@ -9,14 +9,14 @@ public class Database
     private static MySqlConnection connection = new MySqlConnection("Server=localhost;Database=banana;Uid=root;Pwd=Chamex13;");
     public static async Task Gravar(LeituraModel leitura)
     {
-        var sql = "INSERT INTO leitura_ambiente (ID, TEMPERATURA, UMIDADE) VALUES (?, ?, ?)";
+        var sql = "INSERT INTO leitura_ambiente (ID, temperature, humidity) VALUES (?, ?, ?)";
 
         await connection.ExecuteAsync(sql, new
         {
-            ID = Guid.NewGuid(),
-            TEMPERATURA = leitura.temperature,
-            UMIDADE = leitura.humidity
-        });;
+            ID = Guid.NewGuid().ToString(),
+            leitura.temperature,
+            leitura.humidity
+        });
     }
 
     public static async Task<IEnumerable<LeituraModel>> GetAll()
